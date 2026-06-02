c7cc027dc7f7f1699fd044275bf8fd9588e1d47b
HamzaAmir1470 <hamzaamir9733@gmail.com>
2026-06-02 12:53:09 +0500

    Changed setting page header

--- FrontEnd/src/pages/Shop/ShopSettingsPage.jsx
+++ FrontEnd/src/pages/Shop/ShopSettingsPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from '../../components/Layout/Header';
 import Footer from '../../components/Layout/Footer';
 import ShopSettings from '../../components/Shop/ShopSettings.jsx';
+import DashboardHeader from '../../components/Shop/Layout/DashboardHeader.jsx';
 
 const ShopSettingsPage = () => {
     return (
         <div>
-            <Header />
+            <DashboardHeader />
             <ShopSettings />
             <Footer />
         </div>
