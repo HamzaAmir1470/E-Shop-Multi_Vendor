b8b4a7b7f0bcf5b2491138ba3b90b43dd32c8116
HamzaAmir1470 <hamzaamir9733@gmail.com>
2026-06-12 14:17:53 +0500

    Done

--- FrontEnd/src/server.js
+++ FrontEnd/src/server.js
@@ -1,4 +1,4 @@
 export const server = "http://localhost:8000/api/v2";
 export const serverShop = "http://localhost:8000/api/v2/shop";
-export const backend_url = "https://e-shop-multi-vendor.vercel.app/";
+export const backend_url = "http://localhost:8000/";
 export const frontend_url = "http://localhost:5173/";
