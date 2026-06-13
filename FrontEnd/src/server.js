ad0519ff485efe5a5bbc4fe63e4326f8c064ae96
HamzaAmir1470 <hamzaamir9733@gmail.com>
2026-06-13 22:26:54 +0500

    done fix

--- FrontEnd/src/server.js
+++ FrontEnd/src/server.js
@@ -1,4 +1,4 @@
 export const server = "https://e-shop-multi-vendor.vercel.app/api/v2";
 export const serverShop = "https://e-shop-multi-vendor.vercel.app/api/v2/shop";
 export const backend_url = "https://e-shop-multi-vendor.vercel.app/";
-export const frontend_url = "https://sultanf.vercel.app/";
+export const frontend_url = "https://sultanf.vercel.app";
